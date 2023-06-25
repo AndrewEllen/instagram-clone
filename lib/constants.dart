7259771e5d27a61fa67140f57ccb12520a424890
Andrew Ellen <18004410@uhi.ac.uk>
2023-06-25 16:45:53 +0100

    Added constants, navbar and pages

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextStyle defaultTextStyle = const TextStyle(
+  color: Colors.black,
+);
+
+Color appPrimaryColour = Colors.white;
+Color appSecondaryColour = Colors.blueAccent;
+Color appTertiaryColour = Colors.black;
