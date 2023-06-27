fcaeb7c6d185bdef09f5000acd9a1f38505305b7
LewisBrogan <contact@lewisbrogan.co.uk>
2023-06-27 14:20:39 +0100

    loading screen finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/pages/LoadingScreen/loading_screen.dart';
 import 'package:instagram_clone/pages/main_page.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainPage(),
+      home: const LoadingScreen(),
     );
   }
 }
