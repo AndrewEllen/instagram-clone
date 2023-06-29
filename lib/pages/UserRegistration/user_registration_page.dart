bd501d72c87ee01282c04f1707bc389361496426
LewisBrogan <contact@lewisbrogan.co.uk>
2023-06-29 14:58:10 +0100

    Fixed some app hanging issues and colour scheme

--- lib/pages/UserRegistration/user_registration_page.dart
+++ lib/pages/UserRegistration/user_registration_page.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/pages/Login/user_login_page.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Instagram Signup',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SignupPage(),
-    );
-  }
-}
+import 'user_registration_confirmation_page.dart';
 
 class SignupPage extends StatefulWidget {
-  const SignupPage({super.key});
+  const SignupPage({Key? key}) : super(key: key);
 
   @override
   SignupPageState createState() => SignupPageState();
@@ -29,44 +12,68 @@ class SignupPageState extends State<SignupPage> {
   final _phoneNumberController = TextEditingController();
 
   @override
+  void dispose() {
+    _phoneNumberController.dispose();
+    super.dispose();
+  }
+
+  void _navigateToEnterConfirmationPage(BuildContext context) {
+    String phoneNumber = _phoneNumberController.text;
+    if (phoneNumber.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please enter a phone number.'),
+        ),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => EnterConfirmationPage(phoneNumber: phoneNumber),
+        ),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const LoginPage()),
-            );
+            Navigator.pop(context);
           },
         ),
+        backgroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        padding: const EdgeInsets.all(40.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             const Text(
               "What's your mobile number?",
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 24.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 8),
+            const SizedBox(height: 30.0),
             const Text(
               "Enter the mobile number on which you can be contacted. No one will see this on your profile.",
               style: TextStyle(
                 fontSize: 16,
               ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 20.0),
             TextField(
               controller: _phoneNumberController,
               keyboardType: TextInputType.phone,
               decoration: const InputDecoration(
                 labelText: "Mobile number",
+                labelStyle: TextStyle(color: Colors.grey),
                 border: OutlineInputBorder(),
               ),
             ),
@@ -78,13 +85,14 @@ class SignupPageState extends State<SignupPage> {
                 color: Colors.grey,
               ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 30.0),
             Center(
               child: ElevatedButton(
-                onPressed: () {
-                  // TODO: Add functionality here
-                },
+                onPressed: () => _navigateToEnterConfirmationPage(context),
                 child: const Text("Next"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
               ),
             ),
             Center(
@@ -92,6 +100,9 @@ class SignupPageState extends State<SignupPage> {
                 onPressed: () {
                   // TODO: Add Functionality
                 },
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.blue),
+                ),
                 child: const Text("Sign up with email address"),
               ),
             ),
