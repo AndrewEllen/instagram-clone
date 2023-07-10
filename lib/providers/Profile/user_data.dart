58ca57d04358d47e50d8eeee5a7ca12d4f841dee
LewisBrogan <contact@lewisbrogan.co.uk>
2023-07-10 09:18:38 +0100

    Added Pronouns to User Data Provider

--- lib/providers/Profile/user_data.dart
+++ lib/providers/Profile/user_data.dart
@@ -4,9 +4,11 @@ class UserData with ChangeNotifier {
 
   String _userName = "User Name";
   String _userDisplayName = "User Display Name";
+  String _pronouns = "Pronoun";
 
   String get userName => _userName;
   String get userDisplayName => _userDisplayName;
+  String get pronouns => _pronouns;
 
   void updateUserName(String newUserName) {
     _userName = newUserName;
@@ -20,4 +22,9 @@ class UserData with ChangeNotifier {
     notifyListeners();
   }
 
+  void updatePronouns(String newPronouns) {
+    _pronouns = newPronouns;
+
+    notifyListeners();
+  }
 }
